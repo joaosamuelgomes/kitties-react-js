5509769566bf72e2ac6d962edf4c2a5a08bdbcf8
João Samuel Gomes <joaosamuelg001@gmail.com>
2022-02-03 19:27:31 -0300

    Estilização do form para o cadastro e correçãp de cores

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import useForm from '../../Hooks/useForm';
+// import Error from '../../Helper/Error';
 import Button from '../Forms/Button';
+//import UserContext from "../../UserContext"
 import Input from '../Forms/Input';
+import styles from "./LoginForm.module.css"
+import stylesBtn from "../Forms/Button.module.css"
+
 
 const LoginForm = () => {
     const username = useForm()
     const password = useForm()
+
+    // const { userLogin, error, loading } = React.useContext(UserContext);
 
     function handleSubmit(event) {
         event.preventDefault()
@@ -14,14 +21,27 @@ const LoginForm = () => {
     }
 
     return (
-        <section>
-            <h1>Login</h1>
-            <form action="" onSubmit={handleSubmit}>
+        <section className='animeRight'>
+            <h1 className='title'>Login</h1>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <Input label="Usuário" type="text" name="username" {...username}/>
                 <Input label="Senha" type="password" name="password" {...password}/>
+                {/* {loading ? (
+                    <Button disabled>Carregando...</Button>
+                ): (
+                    <Button>Entrar</Button>
+                )} */}
+                <Button>Entrar</Button>
+
+                {/* <Error error={error} /> */}
             </form>
-            <Button>Entrar</Button>
-            <Link to="/login/create">Cadastro</Link>
+            <Link className={styles.forget} to="/login/forget">Esqueceu a senha?</Link>
+            <div className={styles.cadastro}>
+                <h2 className={styles.subtitle}>Cadastre-se</h2>
+                <p>Ainda não possui uma CatConta? Cadastre-se.</p>
+                <Link className={stylesBtn.button} to="/login/create">Cadastro</Link>
+            </div>
+            
 
 
         </section>
