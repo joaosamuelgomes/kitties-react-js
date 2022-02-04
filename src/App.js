9eb9c4e1e0d03bd16f62a47438e0f8ddf44bb591
João Samuel Gomes <joaosamuelg001@gmail.com>
2022-02-03 22:30:06 -0300

    Implementando API

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home"
 import Login from "./Components/Login/Login";
+import NotFound from "./Components/NotFound";
+import { UserStorage } from "./UserContext";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Header />
-      <main className="AppBody">
-      <Routes>
-        <Route path="/" element={<Home/>} /> 
-        <Route path="/login/*" element={<Login />} />
-      </Routes>
-      </main>
-      <Footer />
+        <UserStorage>
+          <Header />
+            <main className="AppBody">
+              <Routes>
+                <Route path="/" element={<Home/>} /> 
+                <Route path="/login/*" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
