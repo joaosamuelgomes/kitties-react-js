32cb015b93547204f17b3652716630500207fc8d
João Samuel Gomes <joaosamuelg001@gmail.com>
2022-02-04 12:00:45 -0300

    Iniciando desenvolvimento de User + SVG's necessários

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,9 +1,9 @@
 import React from 'react';
-//import UserHeader from './UserHeader';
+import UserHeader from './UserHeader';
 import { Routes, Route } from 'react-router-dom';
-//import Feed from '../Feed/Feed';
-//import UserPhotoPost from './UserPhotoPost';
-//import UserStats from './UserStats';
+import Feed from '../Feed/Feed';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
 import { UserContext } from '../../UserContext';
 import NotFound from '../NotFound';
 import Head from '../Helper/Head';
@@ -14,11 +14,11 @@ const User = () => {
   return (
     <section className="container">
       <Head title="Minha Conta" />
-      {/* <UserHeader /> */}
+      <UserHeader />
       <Routes>
-        {/* <Route path="/" element={<Feed user={data.id} />} />
-        <Route path="postar" element={<UserPhotoPost />} />
-        <Route path="estatisticas" element={<UserStats />} /> */}
+        <Route path="/" element={<Feed user={data.id} />} />
+        <Route path="post" element={<UserPhotoPost />} />
+        <Route path="stats" element={<UserStats />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
---
+++ src/Components/User/UserHeaderNav.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import {ReactComponent as MinhasFotos} from '../../Assets/feedgalery.svg'
+import {ReactComponent as Estatisticas} from '../../Assets/feedstats.svg'
+import {ReactComponent as PublicarFoto} from '../../Assets/feedpublication.svg'
+import {ReactComponent as Sair} from '../../Assets/feedlogout.svg'
+import styles from './UserHeaderNav.module.css'
+
+const UserHeaderNav = () => {
+
+
+    const [mobile, setMobile] = React.useState(null);
+    const {userLogout} = React.useContext(UserContext);
+
+    return (
+        <nav className={styles.nav}>
+            <NavLink to="/account">
+                <MinhasFotos />
+                {mobile && "Galeria"}
+            </NavLink>
+            <NavLink to="/account/stats">
+                <Estatisticas />
+                {mobile && "Estatísticas"}
+            </NavLink>
+            <NavLink to="/account/post">
+                <PublicarFoto />
+                {mobile && "Nova Publicação"}
+            </NavLink>
+            <button onClick={userLogout}><Sair />Sair</button>
+        </nav>
+    );
+};
+
+export default UserHeaderNav;
