4a0a740c0fc7ec5e88ceedbc15c0e00b6379c595
João Samuel Gomes <joaosamuelg001@gmail.com>
2022-02-05 20:46:51 -0300

    Alteração de logo e ajuste de Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,14 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from "./Header.module.css";
 import {ReactComponent as Kitties} from "../Assets/kitties.svg";
+import { UserContext } from '../UserContext';
 
 const Header = () => {
-  return <header className={styles.header}>
+  const { data } = React.useContext(UserContext);
+
+  return (
+    <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-          <Link className={styles.logo} to="/" aria-label='Kitties - Home'> <Kitties /></Link>
-          <Link className={styles.login} to="/login">Login / Cadastro</Link>
+        <Link className={styles.logo} to="/" aria-label="Kitties - Home">
+          <Kitties />
+        </Link>
+        {data ? (
+          <Link className={styles.login} to="/account">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Cadastre-se
+          </Link>
+        )}
       </nav>
-  </header>;
+    </header>
+  );
 };
 
 export default Header;
+
