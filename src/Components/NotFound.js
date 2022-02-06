c6ada19350ad3f9b295fde4b2573a370b0dc27f9
João Samuel Gomes <joaosamuelg001@gmail.com>
2022-02-05 21:05:53 -0300

    Ajustes no componente NotFound

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Button from './Forms/Button';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <div className="container mainContainer">
       <h1 className="title">Erro: 404</h1>
-      <p>Página não encontrada.</p>
+      <p style={{paddingBottom: "1.5rem"}}>A página ou termo que você pesquisou não existe mais.</p>
+      <Link className="Button" to="/" aria-label="Kitties - Home">
+          <Button>Página Inicial</Button>
+      </Link>
     </div>
   );
 };
